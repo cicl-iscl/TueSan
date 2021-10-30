936b4fe003a1aabe4bb686d401b3c667de85e563
Jingwen Li <865581101@qq.com>
2021-10-30 21:51:23 +0200

    migrate Leander's code from notebook 1 to run on server

---
+++ prepare_data.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+import json
+import time
+from string import punctuation
+from tqdm import tqdm
+from edit_distance import SequenceMatcher as align
+import numpy as np
+import networkx as nx
+
+import pickle
+from pathlib import Path
+from logger import logger
+from utils import get_task_data, get_task1_IO
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+# DATA_DIR = Path('/Users/jingwen/Desktop/challenges/sanskrit/Tüsan/')
+DATA_DIR = Path('sanskrit/')  # relative path to local copy
+# DATA_DIR = Path('/data/jingwen/sanskrit/') # on server
+
+TRAIN_JSON = Path(DATA_DIR, 'wsmp_train.json')
+TRAIN_GRAPHML = Path(DATA_DIR, 'graphml_train', 'final_graphml_train')
+TRAIN_PICKLE = Path('train_dataset.pickle')
+
+def read_json(jsonfile):
+	with open(jsonfile, encoding="utf-8") as data_file:
+		data = json.load(data_file)
+		normalised_data = get_task_data(data)  # list of dictionary items from the training set
+		return normalised_data
+
+def get_task1_data(datapoint):
+	sandhied, unsandhied_tokenized = get_task1_IO(datapoint)
+	unsandhied = " ".join(unsandhied_tokenized)
+	# Check punctuation: all punctuations are "'" (avagraha, stands for unpronounced 'a')
+	# p = [char for char in sandhied if char in punctuation and char != "'"]
+	# return p
+	# Remove punctuation from input: for now don't, add "'" as vowel
+	return sandhied, unsandhied, unsandhied_tokenized
+
+def tokenize(sandhied, unsandhied):
+	# Align unsandhied tokens with sandhied sentence
+	# to get split points
+	alignment = align(a = sandhied, b = unsandhied)
+	alignment = alignment.get_matching_blocks()
+	sandhied_indices, unsandhied_indices, _ = zip(*alignment)
+
+	# Remove dummy indices
+	sandhied_indices = sandhied_indices[:-1]
+	unsandhied_indices = unsandhied_indices[:-1]
+
+	# Find indices with spaces -> split
+	split_indices = [index for index, char in enumerate(sandhied) if not char.strip()]
+
+	# Find indices where sandhied/unsandhied sentence is
+	# not aligned -> split here
+	sandhied_pointer = 0
+	unsandhied_pointer = 0
+
+	for sandhied_index, unsandhied_index in zip(sandhied_indices, unsandhied_indices):
+		if sandhied_pointer != sandhied_index or unsandhied_pointer != unsandhied_index:
+			if sandhied_index + 1 not in split_indices and sandhied_index - 1 not in split_indices:
+				split_indices.append(sandhied_index)
+				sandhied_pointer = sandhied_index
+				unsandhied_pointer = unsandhied_index
+
+		sandhied_pointer += 1
+		unsandhied_pointer += 1
+
+	split_indices = [0] + list(sorted(set(split_indices)))
+
+	# Tokenize sandhied sentence:
+	# Split at split indices
+	split_indices = zip(split_indices, split_indices[1:] + [None])
+	tokens = [sandhied[start:stop] for start, stop in split_indices]
+	tokens = [token.strip() for token in tokens if not token.isspace()]
+
+	return tokens
+
+def remove_trailing_syllables(tokens, unsandhied_tokenized):
+	if len(tokens) > len(unsandhied_tokenized) and \
+		len(tokens) > 2 and \
+		unsandhied_tokenized[-1].endswith(tokens[-1]) and \
+		unsandhied_tokenized[:3] == tokens[-2].strip()[:3]:
+		tokens = tokens[:-1]
+
+	return tokens
+
+def merge_single_letters(tokens):
+	merged_tokens = []
+	skip_next = False
+
+	for index, token in enumerate(tokens):
+		if skip_next:
+			skip_next = False
+			continue
+
+		token = token.strip()
+		if len(token.strip()) == 1 and index < len(tokens) - 1:
+			merged_token = token + tokens[index + 1].strip()
+			merged_tokens.append(merged_token)
+			skip_next = True
+		else:
+			merged_tokens.append(token)
+
+	return merged_tokens
+
+def make_labels(tokens):
+	combined_string = "".join(tokens)
+
+	offsets = np.array([len(token) for token in tokens])
+	split_indices = np.cumsum(offsets) - 1
+
+	labels = np.zeros(len(combined_string), dtype=np.int8)
+	labels[split_indices] = 1
+
+	return combined_string, labels
+
+def get_allowed_words(sent_id):
+	graphml_path = Path(TRAIN_GRAPHML, str(str(sent_id) + ".graphml"))
+	with open(graphml_path, mode='rb') as graph_input:
+		graph = nx.read_graphml(graph_input)
+		allowed_words = [node[1]['word'] for node in graph.nodes(data = True)]
+
+	return list(sorted(set(allowed_words)))
+
+
+def load_data(picklefile):
+	dataset = []
+	if TRAIN_PICKLE.is_file():
+		with open(picklefile, 'rb') as data:
+			dataset = pickle.load(data)
+	else:
+		for sent in tqdm(normalised_training_data):
+			# p = get_task1_datent)
+			# if p: logger.debug(p)
+			sent_id = sent['sent_id']
+			sandhied, unsandhied, unsandhied_tokenized = get_task1_data(sent)
+
+			# Filter sents where sandhied >> unsandhied
+			if len(sandhied) / len(unsandhied) > 1.5:
+				# logger.warning(f'sandhied: {sandhied}\n')
+				# logger.warning(f'unsandhied: {unsandhied}\n')
+				continue
+
+			# ---- tokenize ----
+			tokens = tokenize(sandhied, unsandhied)
+			tokens = remove_trailing_syllables(tokens, unsandhied_tokenized)
+			tokens = merge_single_letters(tokens)
+			sandhied_merged, labels = make_labels(tokens)
+			allowed_words = get_allowed_words(sent_id)
+
+			# ---- construct datapoint ----
+			datapoint = {
+				'sandhied_merged': sandhied_merged,
+				'labels': labels,
+				'tokens': tokens,
+				'allowed_words': allowed_words,
+				'unsandhied': unsandhied.split(),
+				'sent_id': sent_id
+			}
+
+			dataset.append(datapoint)
+
+		with open(picklefile, 'wb') as out:
+			pickle.dump(dataset, out)
+	return dataset
+
+
+
+if __name__ == '__main__':
+
+	logger.info("Reading and normalising training data ...")
+	s1 = time.time()
+	normalised_training_data = read_json(TRAIN_JSON)
+	logger.debug(normalised_training_data[0])
+	logger.info(f"Took {time.time()-s1:.2f} seconds.")
+
+	s2 = time.time()
+	dataset = load_data(TRAIN_PICKLE)
+	logger.info(f"Took {time.time()-s2:.2f} seconds to prepare task 1 data.")
+
+	logger.debug(pp.pprint(dataset[0]))
---
+++ utils.py
@@ -0,0 +1,78 @@
+import unicodedata
+
+from logger import logger
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def get_task_data(data):
+	"""
+	Returns the unicode normalised data from the given data
+	Input: Data list
+	Output: List of dictionary items
+	"""
+	normalised_data = list()
+	for (item) in data:  # iterate over the list of json data to operate on the values
+		dict_item = unicode_normalize(item)  # recommended to use unicode normalization
+		# for the sanskrit strings to avoid potential normalization mismatches
+		normalised_data.append(dict_item)  # List containing all the entries in the json file, but after normalization
+
+	return normalised_data
+
+
+def get_task1_IO(json_entry):
+	"""
+	Returns the input (as a string) and the groundtruth (as a list of segmented words) for the task 1:
+	Word segmentation.
+	Input: json object -> Json object for a DCS sentence
+	Output: tuple(String, List) -> A tuple of the sentence (words joined with sandhi)
+			and a flattened list of segmented words
+	"""
+	flattened_list = [val for sublist in json_entry['t1_ground_truth'] for val in sublist]
+	# Flatten the nested list to a single linear list
+
+	return (json_entry["joint_sentence"],flattened_list)
+
+
+def unicode_normalize(dict_item):
+	"""
+	It is recommended to use unicode normalization for the sanskrit strings to avoid potential normalization
+	mismatches.
+	Input:  Dict -> JSON object for a DCS sentence, an element from JSON List
+	Output: Dict -> Same content as the input, but all the unicode strings normalized with NFC
+	"""
+	dict_item["joint_sentence"] = unicodedata.normalize('NFC',dict_item["joint_sentence"])
+	#joint sentence - input for tasks 1 and 3
+	dict_item["segmented_sentence"] = unicodedata.normalize('NFC',dict_item["segmented_sentence"])
+	#segmented sentence - input for task 2
+	dict_item["t1_ground_truth"] = [[unicodedata.normalize('NFC',x) for x in inner]
+									for inner in dict_item["t1_ground_truth"]]
+	#ground truth for task 1 - word segmentation
+	dict_item["t2_ground_truth"] = [[unicodedata.normalize('NFC',inner[0]), inner[1]]
+									for inner in dict_item["t2_ground_truth"]]
+	#ground truth for task 2 - morphological parsing
+	dict_item["t3_ground_truth"] = [[[unicodedata.normalize('NFC',x[0]),
+									  unicodedata.normalize('NFC',x[1]), x[2]]
+									 for x in inner] for inner in dict_item["t3_ground_truth"] ]
+	#ground truth for task 3 - word segmentation and morphological parsing
+
+	return dict_item
+
+
+def check_specific_sent(normalised_data, sent_id):
+	"""For Task 1:
+		 Given sent_id, display the groundtruth tokens and the nodes from the graph.
+	"""
+	for entry in normalised_data:
+		if entry['sent_id'] == sent_id:
+			t1_groundtruth = get_task1_IO(entry)
+			logger.debug(entry['joint_sentence'])
+			logger.debug(t1_groundtruth)
+
+			logger.info(entry["graphml_node_ids"])
+
+			full_nodes = get_all_nodes(sent_id)
+			nodes = get_ground_truth_nodes(entry)
+			logger.debug(pp.pprint(nodes))
+			# pp.pprint(full_nodes)
+			# logger.debug(pp.pprint(full_nodes))
