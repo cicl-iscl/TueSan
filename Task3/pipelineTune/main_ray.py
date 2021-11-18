2507d0b2dd31dd1c19eec5f816b639d6236e5e8f
Jingwen Li <865581101@qq.com>
2021-11-18 15:00:34 +0100

    don't know if it works or not

---
+++ Task3/pipelineTune/main_ray.py
@@ -0,0 +1,242 @@
+import json
+import pickle
+
+import torch
+from torch.utils.data import DataLoader
+
+from helpers import load_data
+
+from uni2intern import internal_transliteration_to_unicode as to_uni
+from generate_dataset import construct_train_dataset
+from index_dataset import index_dataset, train_collate_fn, eval_collate_fn
+from model import build_model, build_optimizer, save_model, load_model
+from training import train
+from predicting import make_predictions
+from helpers import save_task3_predictions
+from scoring import evaluate
+from stemming_rules import evaluate_coverage
+
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+
+from pathlib import Path
+import time
+from logger import logger
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def train_model(
+    search,
+    config,
+    indexer,
+    train_dataloader,
+    eval_dataloader,
+    checkpoint_dir=None,
+):
+
+    translit = config["translit"]
+
+    # Build model
+    logger.info("Build model")
+    model = build_model(search, config, indexer)
+    use_cuda = config["cuda"]
+    use_cuda = use_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    logger.info(f"Using devide: {device}")
+    if use_cuda and torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    model = model.to(device)
+
+    # Build optimizer
+    optimizer = build_optimizer(model, search)
+
+    if checkpoint_dir:
+        model_state, optimizer_state = torch.load(
+            Path(checkpoint_dir, "checkpoint").mkdir(parents=True, exist_ok=True)
+        )
+        model.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
+
+    # Train
+    logger.info("Train\n")
+    # epochs = config["epochs"]
+    epochs = search["epochs"]
+    # criterion = get_loss(config)
+    start = time.time()
+
+    model, optimizer = train(model, optimizer, train_dataloader, epochs, device)
+
+    with tune.checkpoint_dir(epoch) as checkpoint_dir:
+        path = Path(checkpoint_dir, "checkpoint").mkdir(parents=True, exist_ok=True)
+        torch.save((model.state_dict(), optimizer.state_dict()), path)
+    return model, optimizer, start
+
+
+def main(num_samples=10, max_num_epochs=20, gpus_per_trial=2):
+
+    # Define search space
+    search = {
+        "lr": tune.loguniform(1e-4, 1e-1),
+        # "batch_size": tune.choice([32, 64, 128]),
+        "epochs": tune.choice([10, 15, 20, 25]),
+        "momentum": tune.choice([0, 0.9]),
+        "nesterov": tune.choice([True, False]),
+        "max_ngram": tune.choice([6, 7, 8, 9]),
+        "dropout": tune.choice([0, 0.1, 0.2, 0.3]),
+        "hidden_dim": tune.choice([256, 512, 1024]),
+        "embedding_dim": tune.choice([32, 64, 128, 256]),
+    }
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=max_num_epochs,
+        grace_period=1,
+        reduction_factor=2,
+    )
+    reporter = CLIReporter(
+        parameter_columns=["epochs", "lr", "max_ngram", "hidden_dim", "embedding_dim"],
+        metric_columns=["loss", "accuracy", "training_iteration"],
+    )
+
+    # Read config
+    with open("config.cfg") as cfg:
+        config = json.load(cfg)
+
+    translit = config["translit"]
+
+    # Load data
+    logger.info("Load data")
+    train_data = load_data(config["train_path"], translit)
+    eval_data = load_data(config["eval_path"], translit)
+
+    logger.info(f"Loaded {len(train_data)} train sents")
+    logger.info(f"Loaded {len(eval_data)} test sents")
+
+    # Generate datasets
+    logger.info("Generate training dataset")
+    train_data, sandhi_rules, stem_rules, tags, discarded = construct_train_dataset(
+        train_data
+    )
+    logger.info(f"Training data contains {len(train_data)} sents")
+    logger.info(f"Collected {len(sandhi_rules)} Sandhi rules")
+    logger.info(f"Collected {len(stem_rules)} Stemming rules")
+    logger.info(f"Collected {len(tags)} morphological tags")
+    logger.info(f"Discarded {discarded} invalid sents from train data")
+
+    evaluate_coverage(eval_data, stem_rules, logger)
+
+    # logger.info("Stem rules")
+    # for (t_pre, t_suf), (s_pre, s_suf) in stem_rules:
+    #    logger.info(f"{t_pre}, {t_suf} --> {s_pre}, {s_suf}")
+
+    logger.info("Generate evaluation dataset")
+
+    # Build vocabulary and index the dataset
+    indexed_train_data, indexed_eval_data, indexer = index_dataset(
+        train_data, eval_data, sandhi_rules, stem_rules, tags
+    )
+
+    logger.info(f"{len(indexer.vocabulary)} chars in vocab:\n{indexer.vocabulary}\n")
+
+    # Build dataloaders
+    logger.info("Build training dataloader")
+    batch_size = config["batch_size"]
+    train_dataloader = DataLoader(
+        indexed_train_data,
+        batch_size=batch_size,
+        collate_fn=train_collate_fn,
+        shuffle=True,
+    )
+
+    start = time.time()
+    # Tuning
+    result = tune.run(
+        partial(
+            train_model,
+            search=search,
+            config=config,
+            indexer=indexer,
+            train_dataloader=train_dataloader,
+            eval_dataloader=eval_dataloader,
+            checkpoint_dir=None,
+        ),
+        resources_per_trial={"cpu": 8, "gpu": gpus_per_trial},
+        # config=search,
+        num_samples=num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter,
+    )
+
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
+    print(
+        "Best trial final validation accuracy: {}".format(
+            best_trial.last_result["accuracy"]
+        )
+    )
+
+    best_trained_model = build_model(best_trial.config, config, indexer)
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+        if gpus_per_trial > 1:
+            best_trained_model = nn.DataParallel(best_trained_model)
+    best_trained_model.to(device)
+
+    best_checkpoint_dir = best_trial.checkpoint.value
+    model_state, optimizer_state = torch.load(Path(best_checkpoint_dir, "checkpoint"))
+    best_trained_model.load_state_dict(model_state)
+
+    pred_eval(best_trained_model, indexer, device, start, translit=False)
+
+
+def pred_eval(model, indexer, device, start, translit=False):
+
+    # Predictions
+    logger.info("Predict\n")
+    eval_dataloader = DataLoader(
+        indexed_eval_data,
+        batch_size=batch_size,
+        collate_fn=eval_collate_fn,
+        shuffle=False,
+    )
+
+    predictions = make_predictions(
+        model, eval_dataloader, indexer, device, translit=translit
+    )
+
+    duration = time.time() - start
+    logger.info(f"Duration: {duration:.2f} seconds.\n")
+
+    # -----Example prediction-----
+    logger.info(f"Example prediction")
+    idx = 0
+
+    logger.info("Input sentence:")
+    logger.debug(eval_data[idx])
+
+    logger.info("Predicted segmentation:")
+    logger.debug(predictions[idx])
+    # logger.info("Gold segmentation:")
+    # logger.debug(true_unsandhied[idx])
+    # ------------------------------
+
+    # Create submission
+    logger.info("Create submission files")
+    save_task3_predictions(predictions, duration)
+
+    # Evaluation
+    ground_truth = [labels for _, labels in eval_data]
+    scores = evaluate(ground_truth, predictions, task_id="t3")
+
+
+if __name__ == "__main__":
+    main(num_samples=10, max_num_epochs=20, gpus_per_trial=2)
---
+++ Task3/pipelineTune/model.py
@@ -0,0 +1,79 @@
+import os
+
+import torch
+import torch.nn as nn
+
+from segmenter_model import SegmenterModel
+from classifier import Classifier
+
+
+def build_model(search, config, indexer):
+    # Read hyperparameters from config
+    # embedding_dim = config["embedding_dim"]
+    # hidden_dim = config["hidden_dim"]
+    # max_ngram = config["max_ngram"]
+    # dropout = config["dropout"]
+    mode = config["char2token_mode"]
+    embedding_dim = search["embedding_dim"]
+    hidden_dim = search["hidden_dim"]
+    max_ngram = search["max_ngram"]
+    dropout = search["dropout"]
+
+    vocabulary_size = len(indexer.vocabulary)
+    num_sandhi_classes = len(indexer.sandhi_rules) + 1  # Add 1 for padding
+    num_stem_classes = len(indexer.stem_rules) + 1
+    num_tag_classes = len(indexer.tags)
+
+    segmenter = SegmenterModel(
+        vocabulary_size,
+        embedding_dim,
+        hidden_dim,
+        max_ngram,
+        num_sandhi_classes,
+        dropout=dropout,
+    )
+
+    classifier = Classifier(
+        hidden_dim,
+        num_stem_classes,
+        num_tag_classes,
+        char2token_mode=mode,
+        dropout=dropout,
+    )
+
+    model = nn.ModuleDict({"segmenter": segmenter, "classifier": classifier})
+    return model
+
+
+def build_optimizer(model, search):
+    return torch.optim.SGD(
+        model.parameters(),
+        lr=search["lr"],
+        momentum=search["momentum"],
+        nesterov=search["nesterov"],
+    )
+
+
+def save_model(model, optimizer, vocabulary, char2index, index2char, name):
+    info = {
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "vocabulary": vocabulary,
+        "char2index": char2index,
+        "index2char": index2char,
+    }
+    if not os.path.exists("./saved_models"):
+        os.makedirs("./saved_models")
+
+    path = os.path.join(".", "saved_models", name + ".pt")
+    torch.save(info, path)
+
+
+def load_model(name, config, vocabulary):
+    model = build_model(config, vocabulary)
+    checkpoint = torch.load(os.path.join(".", "saved_models", name + ".pt"))
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    model.eval()
+
+    return model
