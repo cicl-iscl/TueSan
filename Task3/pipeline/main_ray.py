52ae09ba5eed6972ff52069d64bd7f37eb5b6081
Jingwen Li <865581101@qq.com>
2021-11-21 08:09:05 +0100

    test tune T3?

--- Task3/pipeline/main_ray.py
+++ Task3/pipeline/main_ray.py
@@ -77,47 +77,47 @@ def pred_eval(
 
 def train_model(config, checkpoint_dir=None):
     # Load data
-    #logger.info("Load data")
+    # logger.info("Load data")
     translit = config["translit"]
-    #test = config["test"]
+    # test = config["test"]
 
-    #if translit:
+    # if translit:
     #    logger.info("Transliterating input")
-    #else:
+    # else:
     #    logger.info("Using raw input")
 
     train_data = load_data(config["train_path"], translit)
     eval_data = load_data(config["eval_path"], translit)
-    #test_data = load_task3_test_data(
+    # test_data = load_task3_test_data(
     #    Path(config["test_path"], "task_3_input_sentences.tsv"), translit
-    #)
+    # )
 
-    #logger.info(f"Loaded {len(train_data)} train sents")
-    #logger.info(f"Loaded {len(eval_data)} eval sents")
-    #logger.info(f"Loaded {len(test_data)} test sents")
+    # logger.info(f"Loaded {len(train_data)} train sents")
+    # logger.info(f"Loaded {len(eval_data)} eval sents")
+    # logger.info(f"Loaded {len(test_data)} test sents")
 
     # Generate datasets
-    #logger.info("Generate training dataset")
+    # logger.info("Generate training dataset")
     train_data, sandhi_rules, stem_rules, tags, discarded = construct_train_dataset(
         train_data
     )
-    #logger.info(f"Training data contains {len(train_data)} sents")
-    #logger.info(f"Collected {len(sandhi_rules)} Sandhi rules")
-    #logger.info(f"Collected {len(stem_rules)} Stemming rules")
-    #logger.info(f"Collected {len(tags)} morphological tags")
-    #logger.info(f"Discarded {discarded} invalid sents from train data")
+    # logger.info(f"Training data contains {len(train_data)} sents")
+    # logger.info(f"Collected {len(sandhi_rules)} Sandhi rules")
+    # logger.info(f"Collected {len(stem_rules)} Stemming rules")
+    # logger.info(f"Collected {len(tags)} morphological tags")
+    # logger.info(f"Discarded {discarded} invalid sents from train data")
 
-    #if not test:
+    # if not test:
     #    evaluate_coverage(eval_data, stem_rules, logger)
 
-    #logger.info("Generate evaluation dataset")  # the same
+    # logger.info("Generate evaluation dataset")  # the same
 
     # Build vocabulary and index the dataset
     indexed_train_data, indexed_eval_data, indexer = index_dataset(
         train_data, eval_data, sandhi_rules, stem_rules, tags
     )
 
-    #logger.info(f"{len(indexer.vocabulary)} chars in vocab:\n{indexer.vocabulary}\n")
+    # logger.info(f"{len(indexer.vocabulary)} chars in vocab:\n{indexer.vocabulary}\n")
 
     # Build dataloaders
     # logger.info("Build training dataloader")
@@ -205,10 +205,12 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
         reporter = CLIReporter(
             parameter_columns=[
                 "epochs",
-                "lr",
-                "max_ngram",
+                "translit",
+                "batch_size",
+                "dropout",
                 "hidden_dim",
                 "embedding_dim",
+                "char2token_mode",
             ],
             metric_columns=["loss", "score", "training_iteration"],
             max_report_frequency=300,  # report every 5 min
@@ -217,14 +219,14 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
         # Tuning
         result = hyperparam_tune.run(
             partial(train_model, checkpoint_dir=config["checkpoint_dir"]),
-            resources_per_trial={"cpu": 8, "gpu": gpus_per_trial},
+            resources_per_trial={"cpu": 7, "gpu": gpus_per_trial},
             config=config,  # our search space
             num_samples=num_samples,
             scheduler=scheduler,
             progress_reporter=reporter,
             name="T3_tune",
             log_to_file=True,
-            fail_fast=True,  # stopping after first failure
+            # fail_fast=True,  # stopping after first failure
             # resume=True
         )
         best_trial = result.get_best_trial("score", "max", "last")
@@ -237,18 +239,18 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
         logger.info(
             "Best trial final task score: {}".format(best_trial.last_result["score"])
         )
-        
+
         config = best_trial.config
         with open("best_config_t3.pickle", "wb") as cf:
             pickle.dump(best_trial.config, cf)
-    
+
     else:
         model, optimizer = train_model(train_config)
 
     # (false) end of prediction
     duration = time.time() - start
     logger.info(f"Duration: {duration:.2f} seconds.\n")
-    device = "cuda" if torch.cuda.is_available() and config['cuda'] else "cpu"
+    device = "cuda" if torch.cuda.is_available() and config["cuda"] else "cpu"
 
     if test:
         # Load data
@@ -269,7 +271,7 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
         indexed_train_data, indexed_eval_data, indexer = index_dataset(
             train_data, eval_data, sandhi_rules, stem_rules, tags
         )
-        
+
         if tune:
             best_trained_model = build_model(best_trial.config, indexer)
             best_trained_model.to(device)
@@ -277,18 +279,22 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
             best_checkpoint_dir = best_trial.checkpoint.value
             model_state, optimizer_state = torch.load(
                 Path(best_checkpoint_dir, "checkpoint")
-                )
+            )
             best_trained_model.load_state_dict(model_state)
             model = best_trained_model
-            
 
         logger.info("Evaluating on eval data")
         eval_dataloader = DataLoader(
-            indexed_eval_data, batch_size=64, collate_fn=eval_collate_fn, shuffle=False,
+            indexed_eval_data,
+            batch_size=64,
+            collate_fn=eval_collate_fn,
+            shuffle=False,
         )
-        scores = pred_eval(model, eval_data, eval_dataloader, indexer, device, translit=translit)
+        scores = pred_eval(
+            model, eval_data, eval_dataloader, indexer, device, translit=translit
+        )
         print_scores(scores)
-        
+
         logger.info("Creating predictions on test data")
         # Index test data
         indexed_test_data = []
@@ -308,7 +314,7 @@ def main(tune, num_samples=10, max_num_epochs=20, gpus_per_trial=1):
         predictions = make_predictions(
             model, test_dataloader, indexer, device, translit=translit
         )
-        
+
         # Create submission
         logger.info("Create submission files")
         save_task3_predictions(predictions, duration)
@@ -322,5 +328,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     tune = args.tune
-    main(tune, num_samples=1, max_num_epochs=20, gpus_per_trial=1)  # test
-    # main(num_samples=20, max_num_epochs=25, gpus_per_trial=1)
+    # main(tune, num_samples=1, max_num_epochs=20, gpus_per_trial=1)  # test
+    main(num_samples=30, max_num_epochs=25, gpus_per_trial=0.5)
